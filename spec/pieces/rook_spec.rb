1645c7c1cd7ee2f05855ee9ff60438cbcfa02542
Erik-Russell <erik.z.russell@gmail.com>
2024-10-18 16:20:10 -0700

    New expected array
    test now only has one rook on the board

--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -1,15 +1,22 @@
+require_relative '../utility.rb'
 require_relative '../../lib/pieces/rook.rb'
 require_relative '../../lib/chessboard.rb'
 
 describe Rook do
+  include Utility
+
   subject(:white_rook) { Rook.new(:white,[7,0]) }
   let(:board) { ChessBoard.new }
+
+  before do
+    setup_board_with_piece(board, white_rook, [7, 0])
+  end
 
   describe '#valid_moves' do
     it 'returns all valid horizontal and vertical moves as an array' do
       expected_moves = [
         [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0],  # Up (north)
-        [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6],          # Right (east)  
+        [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7]   # Right (east)  
       ]
 
       expect(white_rook.valid_moves(board)).to match_array(expected_moves)
