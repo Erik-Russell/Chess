9d521c5eda2f1497449522aee2d2a1c8baf60690
Erik-Russell <erik.z.russell@gmail.com>
2024-10-18 16:04:49 -0700

    Valid Moves update
    valid moves now stops at non-empty squars, buy allows move onto opponent
    piece

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,17 +1,32 @@
 require_relative 'piece'
+require_relative '../chessboard'
 
 class Rook < Piece
-  def valid_moves
+  def valid_moves(board)
     moves = []
 
-    (0..7).each do |col|
-      moves << [position[0], col] unless col == position[1]
-    end
+    directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-    (0..7).each do |row|
-      moves << [row, position[1]] unless row == position[0]
-    end
+    directions.each do |dir|
+      row, col = position
+      loop do
+        row += dir[0]
+        col += dir[1]
+        break unless row.between?(0, 7) && col.between?(0, 7) # stay on board
 
+        target_piece = board.piece_at([row, col])
+
+        if target_piece == ' ' || target_piece == '.' || target_piece.nil?
+          moves << [row, col] # empty squares ==> valid
+        elsif target_piece.is_a?(Piece) && target_piece.color != self.color
+          moves << [row, col] # opponenet color
+          break               # go no further
+        else
+          break # 
+        end
+      end
+    end
+    
     moves
   end
 
