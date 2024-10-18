db2f9c67968ed11a068f773ca8e5dbbc9e87e96e
Erik-Russell <erik.z.russell@gmail.com>
2024-10-18 16:06:01 -0700

    Modified for new Valid_moves

--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -1,15 +1,18 @@
 require_relative '../../lib/pieces/rook.rb'
+require_relative '../../lib/chessboard.rb'
 
 describe Rook do
-  subject(:white_rook) { Rook.new(:white,[0,0]) }
-  it 'returns an array of moves' do
-    expect(white_rook.valid_moves).to be_an(Array)
-  end
+  subject(:white_rook) { Rook.new(:white,[7,0]) }
+  let(:board) { ChessBoard.new }
 
-  it 'returns correct array from a1' do
-    expect(white_rook.valid_moves).to contain_exactly(
-      [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0],  # Vertical moves
-      [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]   # Horizontal moves
-      )
+  describe '#valid_moves' do
+    it 'returns all valid horizontal and vertical moves as an array' do
+      expected_moves = [
+        [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0],  # Up (north)
+        [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6],          # Right (east)  
+      ]
+
+      expect(white_rook.valid_moves(board)).to match_array(expected_moves)
+    end
   end
 end
