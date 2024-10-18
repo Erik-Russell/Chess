8aed13404816f74fa1133de5fd25440771fe6a41
Erik-Russell <erik.z.russell@gmail.com>
2024-10-17 20:11:56 -0700

    Initial

---
+++ lib/pieces/Piece.rb
@@ -0,0 +1,16 @@
+#
+#
+#
+class Piece
+  attr_reader :color, :position
+  
+  def initialize(color, position)
+    @color = color # :white or :black
+    @position = position # array [0, 0] is 'a1'
+  end
+
+  # this should be overridden by subclass
+  def valid_moves
+    raise NotImplementedError, "This method should be overridden by subclasses."
+  end
+end
