b7b8eb97820b04574937006abc38adafa4dda8a6
Erik-Russell <erik.z.russell@gmail.com>
2024-10-18 09:57:23 -0700

    Place Rooks on board

--- lib/chessboard.rb
+++ lib/chessboard.rb
@@ -1,14 +1,18 @@
 #
 #
-#
+Dir[File.join(__dir__, 'lib/pieces' '*.rb')].each { |file| require_relative file }
 #
 class ChessBoard
+  attr_reader :board
+
   def initialize
     @board = Array.new(8) do |row|
       Array.new(8) do |col|
         (row + col).even? ? ' ' : '.'
       end
     end
+
+    place_pieces
   end
 
   def display
@@ -21,4 +25,17 @@ class ChessBoard
     puts "    a   b   c   d   e   f   g   h"
   end
 
+  def place_pieces
+    # white rooks
+    @board[7][0] = Rook.new(:white,[7, 0])
+    @board[7][7] = Rook.new(:white,[7, 7])
+    # black rooks
+    @board[0][0] = Rook.new(:black,[0, 0])
+    @board[0][7] = Rook.new(:black,[0, 7])
+  end
+
+  def piece_at(position)
+    row, col = position
+    @board[row][col]
+  end
 end
