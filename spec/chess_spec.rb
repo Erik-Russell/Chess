d5f79b83ffc484ea862999998847c38be6084e08
Erik-Russell <erik.z.russell@gmail.com>
2024-10-20 11:01:06 -0700

    Draft of Chess#player_move test

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -11,6 +11,23 @@ describe Chess do
       expect(game).to be_an_instance_of(Chess)
     end
 
+    it 'creates a new chessboard' do
+      expect(game.board).to be_an_instance_of(ChessBoard)
+    end
+
+  end
+
+  describe '#player_move' do
+    subject(:game) { Chess.new }
+
+    it 'makes an array of valid_moves' do
+      position = [7, 1] # white queen's knight
+      expected_moves = [
+        [5, 0], [5, 2]
+      ]
+
+      expect(game.player_move(position)).to match_array(expected_moves)
+    end
   end
 
 end
