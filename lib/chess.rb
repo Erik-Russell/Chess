06bf8d75d56bc72cc7de28f43d6611777e48252a
Erik-Russell <erik.z.russell@gmail.com>
2024-10-20 11:00:30 -0700

    Draft of Chess#player_move

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,6 +1,19 @@
 #
 #
+require_relative './chessboard.rb'
 #
 class Chess
+  attr_reader :board
 
+  def initialize
+    @board = ChessBoard.new
+  end
+
+  def player_move(position)
+    ## row, col = position
+    # if valid_move
+    piece = @board.piece_at(position)
+    moves = piece.valid_moves(@board)
+    # move_piece
+  end
 end
