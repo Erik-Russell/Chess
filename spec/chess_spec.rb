a231f427f374b11ae458cfe6ac6f5b172a286e28
Erik-Russell <erik.z.russell@gmail.com>
2024-10-23 07:39:03 -0700

    More tests on Chess

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -27,5 +27,29 @@ describe Chess do
 
       expect(game.board.piece_at([2, 2])).to be_an_instance_of(Knight)
     end
+
+    it "moves a white pawn to d4" do
+      move = 'd4'
+      game.player_move(move, :white)
+      game.board.display
+
+      expect(game.board.piece_at([4, 3])).to be_an_instance_of(Pawn)
+    end
+
+    context "simple opening" do
+      it 'setup basic pawn triangle' do
+        game.player_move('d4', :white)
+        game.player_move('d5', :black)
+        game.player_move('e4', :white)
+        game.player_move('e5', :black)
+        game.player_move('c6', :white)
+        game.player_move('c3', :black)
+        game.player_move('f6', :white)
+        game.player_move('f3', :black)
+        game.board.display
+
+        expect(game.board.piece_at([0,0])).to be_an_instance_of(Rook)
+      end
+    end
   end
 end
